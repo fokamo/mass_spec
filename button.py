5cee4e6fa834ec76c6caa1ecb1ce04bdc245aa6b
fokamo <52177356+fokamo@users.noreply.github.com>
2021-05-27 13:24:05 -0700

    use Text class

--- button.py
+++ button.py
@@ -1,20 +1,20 @@
 """ button.py: for a Button class """
 
 import pygame
+import text
 
 # required initialization step
 pygame.init()
 
 # font constants
 BUTTON_FONT = pygame.font.SysFont('calibri', 35)
-FONT_COLOR = pygame.Color(0, 0, 0)
 
 class Button(pygame.Rect):
     """A button which can draw itself and be clicked"""
 
     def __init__(self, left: int, top: int, width: int, height: int,
-                 text: str, color: pygame.Color):
-        """ Initializes a Button
+                 txt: str, color: pygame.Color):
+        """ Initialize a Button
 
         left, top, width, and height determine the size of the Button
         text is the Button's label, color is the Button's background color
@@ -23,18 +23,11 @@ class Button(pygame.Rect):
         super().__init__(left, top, width, height)
         
         # make a text-surface to be blitted later
-        self.__text = BUTTON_FONT.render(text, True, FONT_COLOR, color)
-        
-        # temporary text Rect, used to grab width/height for centering text
-        temp_rect = self.__text.get_rect()
-        self.__text_pos = (left + (width / 2) - (temp_rect.width / 2),
-                           top + (height / 2) - (temp_rect.height / 2)
-                           )
-                                       
+        self.__text = text.Text(txt, BUTTON_FONT, self, color)
         self.__color = color
 
     def is_clicked(self, mouse_x: int, mouse_y: int) -> bool:
-        """ Check for button clicks """
+        """ Check if a given mouse click is on the button """
         
         return super().collidepoint(mouse_x, mouse_y)
 
@@ -43,4 +36,4 @@ class Button(pygame.Rect):
 
         # draw rectangle then text on top
         pygame.draw.rect(screen, self.__color, self)
-        screen.blit(self.__text, self.__text_pos)
+        self.__text.draw(screen)
