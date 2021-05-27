1d1f8f120f916e07a3fe477d9e85a182b1d4cb59
fokamo <52177356+fokamo@users.noreply.github.com>
2021-05-27 13:25:34 -0700

    Add in titles & shuffle logic

--- runner.py
+++ runner.py
@@ -7,7 +7,7 @@ Info has several sub-screens with information.
 """
 
 import pygame, sys
-import button
+import button, text
 
 # required initialization step
 pygame.init()
@@ -16,14 +16,17 @@ pygame.init()
 FPS = 30
 game_clock = pygame.time.Clock()
 
-# set up color constants
+# color constants
 BLUE = pygame.Color(38, 228, 235)
 RED = pygame.Color(235, 91, 91)
+
+# font constants
+TITLE_FONT = pygame.font.SysFont('inkfree', 60)
+SUBTITLE_FONT = pygame.font.SysFont('inkfree', 20, False, True)
 
 # set up the display screen
 WINDOW_SIZE = (1000, 600)
 window = pygame.display.set_mode(WINDOW_SIZE)
-window.fill(BLUE)
 # not quite sure what this does - need to check documentation
 # background = pygame.Surface(WINDOW_SIZE)
 
@@ -35,15 +38,26 @@ def main():
 
     # initially on start screen
     screen = START
+    background_color = BLUE
+    window.fill(background_color)
 
+    # set up UI elements
     exit_button = button.Button(450, 500, 100, 50, "Exit", RED)
-
+    title = text.Text("Mass Spectrometer", TITLE_FONT,
+                      pygame.Rect(0, 0, WINDOW_SIZE[0], WINDOW_SIZE[1] / 2),
+                      background_color)
+    subtitle = text.Text("by Faith Okamoto", SUBTITLE_FONT,
+                         pygame.Rect(0, 100, WINDOW_SIZE[0],
+                                     WINDOW_SIZE[1] / 2), background_color)
+                                  
     # game loop
     while True:
         if screen == START:
             pygame.display.set_caption("Start")
 
             exit_button.draw(window)
+            title.draw(window)
+            subtitle.draw(window)
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
                     mouse_x, mouse_y = pygame.mouse.get_pos()
