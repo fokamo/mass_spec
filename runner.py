06d486a6479a95643177bc7a86920b00f8332c90
fokamo <52177356+fokamo@users.noreply.github.com>
2021-05-26 22:03:58 -0700

    started adding a Button (currently broken)

--- runner.py
+++ runner.py
@@ -7,6 +7,7 @@ Info has several sub-screens with information.
 """
 
 import pygame, sys
+import button
 
 # required initialization step
 pygame.init()
@@ -17,13 +18,13 @@ game_clock = pygame.time.Clock()
 
 # set up color constants
 BLUE = pygame.Color(38, 228, 235)
+RED = pygame.Color(235, 91, 91)
 
 # set up the display screen
 WINDOW_SIZE = (1000, 600)
 window = pygame.display.set_mode(WINDOW_SIZE)
 window.fill(BLUE)
-# surface - not needed until something is drawn
-# background = pygame.Surgace(WINDOW_SIZE)
+background = pygame.Surface(WINDOW_SIZE)
 
 def main():
     """Main runner function. Implements high-level logic"""
@@ -34,20 +35,26 @@ def main():
     # initially on start screen
     screen = START
 
+    exit_button = button.Button(500, 450, 50, 100, "Exit", RED)
+
     while True:
+        print("hello")
         if screen == START:
             pygame.display.set_caption("Start")
-            
+
+            exit_button.draw(background)
             for event in pygame.event.get():
                 if event.type == pygame.MOUSEBUTTONDOWN:
-                    # must do both to exit properly
-                    pygame.quit()
-                    sys.exit()
+                    if exit_button.is_clicked(pygame.mouse.get_pos()):
+                        # must do both to exit properly
+                        pygame.quit()
+                        sys.exit()
+                        
         # update screen & tick clock
         pygame.display.update()
         game_clock.tick(FPS)
     
 
-# call the "main: function if running this script
+# call the "main" function if running this script
 if __name__ == "__main__":
     main()
