14e121d06183f5f9511f954632a19b06f4e14074
fokamo <52177356+fokamo@users.noreply.github.com>
2021-05-28 08:45:03 -0700

    added comments

--- info_section.py
+++ info_section.py
@@ -1,19 +1,30 @@
 """ info_section.py: for an InfoSection class """
 
 class InfoSection():
+    """ InfoScection class for tidy storage of each page's text """
+    
     def __init__(self, title: str, info: list, sources: list):
         self.title = title 
         self.info = info
         self.sources = sources
 
 def load_info_sections(filename: str):
+    """ Loads data from a file into InfoSections """
+    
     info_sections = []
+    
     with open(filename) as file:
         lines = file.readlines()
+        
         for line in lines:
+            # different sections of data are separated by ;
             data = line.split(";")
+            
             title = data[0]
+            # paragraphs are separated by ,
             info = data[1].split(",")
             sources = data[2].split(",")
+            
             info_sections.append(InfoSection(title, info, sources))
+            
     return info_sections
