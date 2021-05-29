ef730be6c3265910a76097b4a1157e279d3311a4
fokamo <52177356+fokamo@users.noreply.github.com>
2021-05-29 15:04:23 -0700

    reset capability

--- mass_spectrometer.py
+++ mass_spectrometer.py
@@ -12,10 +12,13 @@ WALL_COLOR = pygame.Color(148, 134, 106)
 class MassSpectrometer():
 
     def __init__(self, area: pygame.Rect, mass: int, charge: int,
-                 x_velocity: int, e_field: int, mag_field: int):
-        self.__particle = charged_particle.ChargedParticle(
-            mass, charge, x_velocity, (area.left + charged_particle.RADIUS,
-                                       area.top + (area.height / 2)))
+                 initial_x_velocity: int, e_field: int, mag_field: int):
+        self.__area = area
+        self.__mass = mass
+        self.__charge = charge
+        self.__initial_x_velocity = initial_x_velocity
+        self.__particle_start_pos = (charged_particle.RADIUS, area.height / 2)
+        self.reset_particle()
         self.e_field = e_field
         self.mag_field = mag_field
         self.__walls = self.__generate_walls(area)
@@ -42,3 +45,8 @@ class MassSpectrometer():
         
         for wall in self.__walls:
             pygame.draw.rect(screen, WALL_COLOR, wall)
+
+    def reset_particle(self):
+        self.__particle = charged_particle.ChargedParticle(
+            self.__mass, self.__charge, self.__initial_x_velocity,
+            self.__particle_start_pos)
