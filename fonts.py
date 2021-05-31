5b48865bd3bbd70605624afba68f1e2677c1cd9d
fokamo <52177356+fokamo@users.noreply.github.com>
2021-05-30 22:07:49 -0700

    added NUMBER_FONT

--- fonts.py
+++ fonts.py
@@ -1,4 +1,4 @@
-""" fonts.py: for font constants across the simulations """
+""" fonts.py: for font constants across the simulation """
 
 import pygame
 
@@ -9,3 +9,4 @@ TITLE_FONT = pygame.font.SysFont('inkfree', 60)
 SUBTITLE_FONT = pygame.font.SysFont('inkfree', 20, False, True)
 BUTTON_FONT = pygame.font.SysFont('calibri', 35)
 PARAGRAPH_FONT = pygame.font.SysFont('arial', 15)
+NUMBER_FONT = pygame.font.SysFont('arial', 10)
