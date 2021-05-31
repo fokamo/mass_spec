6f5eadb06fcd9bffaaf27130dfc0b2ffe1c76467
fokamo <52177356+fokamo@users.noreply.github.com>
2021-05-31 16:18:58 -0700

    DiscreteSlider subclass

--- slider.py
+++ slider.py
@@ -1,6 +1,7 @@
 """ slider.py: for a Slider class """
 
 import pygame
+from math import fabs
 import text, fonts
 
 # required initialization step
@@ -25,10 +26,10 @@ class Slider():
                                    area.width, area.height / 4)
         self.__circle_radius = area.height / 8
         self.__range = val_range
-        self.__set_value(initial_value)
+        self.set_value(initial_value)
         self.__labels = self.__generate_labels(background_color)
 
-    def __set_value(self, new_val: int):
+    def set_value(self, new_val: int) -> int:
         min_val, max_val = self.__range
         if new_val < min_val or new_val > max_val:
             raise ValueError('Value must be between', min_val, 'and', max_val)
@@ -36,6 +37,7 @@ class Slider():
         self.__circle_pos = (self.__slide.left + (self.__slide.width * percent),
                              self.__slide.top + (self.__slide.height / 2))
         self.__value = new_val
+        return new_val
 
     def __generate_labels(self, background_color: pygame.Color):
         labels = []
@@ -69,6 +71,36 @@ class Slider():
             percent = (mouse_x - self.__slide.left) / self.__slide.width
             new_val = self.__range[0] + (
                 (self.__range[1] - self.__range[0]) * percent)
-            self.__set_value(new_val)
-            return new_val
+            return self.set_value(new_val)
         return None
+
+class DiscreteSlider(Slider):
+    
+    def __init__(self, name: str, val_range: (int, int), area: pygame.Rect,
+                 background_color: pygame.Color, initial_value: int):
+        super().__init__(name, val_range, area, background_color, initial_value)
+        self.__allowed_vals = []
+        self.__allowed_x_coords = []
+        val_jump = (val_range[1] - val_range[0]) / (TICKS - 1)
+        x_jump = area.width / (TICKS - 1)
+        for i in range(TICKS):
+            self.__allowed_vals.append(val_range[0] + (val_jump * i))
+            self.__allowed_x_coords.append(area.left + (x_jump * i))
+
+    def __set_value(self, new_val: int):
+        if new_val not in self.__allowed_vals:
+            raise ValueError("Not an allowed discrete value")
+        else:
+            super().set_value(new_val)
+
+    def handle_click(self, mouse_x: int, mouse_y: int) -> int:
+        least_x_diff = float('inf')
+
+        if self.is_clicked(mouse_x, mouse_y):
+            for i in range(len(self.__allowed_x_coords)):
+                x_coord = self.__allowed_x_coords[i]
+                diff = fabs(x_coord - mouse_x)
+                if diff < least_x_diff:
+                    least_x_diff = diff
+                    closest_index = i
+            return self.set_value(self.__allowed_vals[closest_index])
