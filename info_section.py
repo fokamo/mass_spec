d35b4ccda80da01a0009a87478643de2d33f67e2
fokamo <52177356+fokamo@users.noreply.github.com>
2021-06-02 13:06:57 -0700

    documentation

--- info_section.py
+++ info_section.py
@@ -1,22 +1,47 @@
-""" info_section.py: for an InfoSection class """
+"""info_section.py: for an InfoSection class
+
+InfoSection class -- for storing repetitive informational pages
+load_info_sections -- to load informational sections from a file
+"""
 
 class InfoSection():
-    """ InfoScection class for tidy storage of each page's text """
+    """A class to represent an informational page
+
+    Attributes:
+    title -- title of the page
+    info -- list of informational paragraphs on the page
+    source -- source of the page
+    """
     
-    def __init__(self, title: str, info: list, source: str):
+    def __init__(self, title: str, info: list, source: str) -> None:
+        """Initialize an InfoSection.
+
+        title -- title of the page
+        info -- list of informational paragraphs on the page
+        source -- source of the page
+        """
+        
         self.title = title 
         self.info = info
         self.source = "Source: " + source
 
-def load_info_sections(filename: str):
-    """ Loads data from a file into InfoSections """
-    
+def load_info_sections(filename: str) -> list:
+    """Load data from a file into InfoSections.
+
+    filename -- string with file path
+
+    Returns a list of InfoSections
+    """
+
+    # return variable
     info_sections = []
     
     with open(filename, encoding='utf8') as file:
-        lines = file.readlines()
         
-        for line in lines:
+        for line in file.readlines():
+            # remove excess whitespace
+            line.strip()
+            
             # different sections of data are separated by |
             data = line.split("|")
             
@@ -24,7 +49,8 @@ def load_info_sections(filename: str):
             # paragraphs are separated by ;;
             info = data[1].split(";;")
             sources = data[2]
-            
+
+            # create InfoSection based off data
             info_sections.append(InfoSection(title, info, sources))
             
     return info_sections
