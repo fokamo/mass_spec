10ad2ce0e29a685164347f43d21815f797eafc8c
fokamo <52177356+fokamo@users.noreply.github.com>
2021-06-02 12:36:09 -0700

    style changes

--- button.py
+++ button.py
@@ -7,7 +7,7 @@ import text, fonts
 pygame.init()
 
 class Button(pygame.Rect):
-    """A class to represent a button. Subclass of pygame.Rect
+    """A class to represent a button. Subclass of pygame.Rect.
 
     Attributes:
     left, top, width, height -- from Rect
@@ -16,12 +16,12 @@ class Button(pygame.Rect):
 
     Methods:
     is_clicked -- checks if a given mouseclick-point is on the Button
-    draw -- draws the Button on a surface
+    draw -- draws the Button on a Surface
 
     In all cases the origin (0, 0) is the top left corner
     """
 
-    def __init__(self, txt: str, area: pygame.Rect, color: pygame.Color
+    def __init__(self, label: str, area: pygame.Rect, color: pygame.Color
                  ) -> None:
         """Initialize a Button.
 
@@ -33,7 +33,7 @@ class Button(pygame.Rect):
         super().__init__(area)
         
         # make a text-surface to be blitted later
-        self._text = text.Text(txt, fonts.BUTTON_FONT, self, color)
+        self._text = text.Text(label, fonts.BUTTON_FONT, self, color)
         self._color = color
 
     def is_clicked(self, mouse_x: int, mouse_y: int) -> bool:
@@ -41,12 +41,14 @@ class Button(pygame.Rect):
 
         mouse_x -- the x coordinate of the mouseclick
         mouse_y -- the y coordinate of the mouseclick
+
+        Returns a bool -- True if the click is on the Button, False if not
         """
         
         return super().collidepoint(mouse_x, mouse_y)
 
     def draw(self, screen: pygame.Surface) -> None:
-        """Draw the Button onto a given Surface"""
+        """Draw the Button onto a given Surface."""
 
         # draw rectangle then text on top
         pygame.draw.rect(screen, self._color, self)
